1e06b1eaeff9c4b189b050fb377594025bbc016d
MirnaMarie <mariya.tikhom@gmail.com>
2024-06-25 22:07:56 +0300

    Обновила настройки проекта

--- Django/manage.py
+++ Django/manage.py
@@ -9,6 +9,9 @@ Command-line utility for administrative tasks.
 import os
 import sys
 
+def new_func():
+    execute_from_command_line(sys.argv)
+
 if __name__ == '__main__':
     os.environ.setdefault(
         'DJANGO_SETTINGS_MODULE',
@@ -21,4 +24,4 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    new_func()
